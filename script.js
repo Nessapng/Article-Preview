8e0ba4b70b963b4ddb27aaeb49bf044f904549ba
Nessapng <197740425+Nessapng@users.noreply.github.com>
2025-04-14 10:33:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const shareBtn = document.querySelector(".share-button");
+  
+    shareBtn.addEventListener("click", () => {
+      // Simple interaction
+      alert("Sharing options coming soon!")
+    });
+});
